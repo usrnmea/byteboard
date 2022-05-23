e4ed7182c75ef286b6efec93c06dda42e126a981
usrnmea <53175704+usrnmea@users.noreply.github.com>
2022-05-23 14:13:05 +0300

    bit_scan_reverse and bit_scan_forward unit tests fix

--- test/test_bitboard.c
+++ test/test_bitboard.c
@@ -3,14 +3,14 @@
 
 void test_bit_scan_forward(void)
 {
-	TEST_ASSERT_EQUAL_UINT32(1, bit_scan_forward(UNIVERSE));
-	TEST_ASSERT_EQUAL_UINT32(3, bit_scan_forward(0x10ull));
-	TEST_ASSERT_EQUAL_UINT32(63, bit_scan_forward(0x0100000000000000ull));
+	TEST_ASSERT_EQUAL_UINT32(0, bit_scan_forward(UNIVERSE));
+	TEST_ASSERT_EQUAL_UINT32(4, bit_scan_forward(0x10ull));
+	TEST_ASSERT_EQUAL_UINT32(56, bit_scan_forward(0x0100000000000000ull));
 }
 
 void test_bit_scan_reverse(void)
 {
 	TEST_ASSERT_EQUAL_UINT32(63, bit_scan_reverse(UNIVERSE));
-	TEST_ASSERT_EQUAL_UINT32(3, bit_scan_reverse(0x10ull));
-	TEST_ASSERT_EQUAL_UINT32(61, bit_scan_reverse(0x0100000000000000ull));
+	TEST_ASSERT_EQUAL_UINT32(4, bit_scan_reverse(0x10ull));
+	TEST_ASSERT_EQUAL_UINT32(56, bit_scan_reverse(0x0100000000000000ull));
 }
