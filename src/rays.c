aa2a76c7d7582444ff1189200c25882abbceabab
Gaiercop <gaiercop@gmail.com>
2022-05-24 10:07:31 +0300

    rays.c: fixed typo && assert.h include

--- src/rays.c
+++ src/rays.c
@@ -1,11 +1,13 @@
 #include "bitboard.h"
 #include "bitboard_mapping.h"
 
+#include <assert.h>
+
 //RAYS
 //Returns a ray between 2 squares
 U64 in_between(Square first, Square second) {
-	assert(first < SQUARE_NB);
-	assert(second < SQUARE_NB);
+	assert(first < SQ_NB);
+	assert(second < SQ_NB);
 
 	const U64 m1   = -1ULL;
 	const U64 a2a7 = 0x0001010101010100ULL;
@@ -26,31 +28,31 @@ U64 in_between(Square first, Square second) {
 
 // This is the function for calculating the west ray
 U64 get_ray_west(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 	return (0x01ULL << square) - (0x01ULL << (square & 56));
 }
 
 // This is the function for calculating the south ray
 U64 get_ray_south(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 	return 0x0080808080808080ULL >> (square ^ 63);
 }
 
 // This is the function for calculating the east ray
 U64 get_ray_east(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 	return 2 * ((0x01ULL << (square | 7)) - (0x01ULL << square) );
 }
 
 // This is the function for calculating the north ray
 U64 get_ray_north(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 	return 0x0101010101010100ULL << square;
 }
 
 // This is a function for calculating a diagonal ray(including square)
 U64 get_ray_diagonal(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 
 	int32_t diagonal = 8 * (square & 7) - (square & 56);
 	int32_t nort = -diagonal & (diagonal >> 31);
@@ -60,7 +62,7 @@ U64 get_ray_diagonal(Square square) {
 
 // This is a function for calculating a anti diagonal ray(including square)
 U64 get_ray_anti_diagonal(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 
 	int32_t diagonal = 56 - 8 * (square & 7) - (square & 56);
 	int32_t north = -diagonal & ( diagonal >> 31);
@@ -70,24 +72,24 @@ U64 get_ray_anti_diagonal(Square square) {
 
 // This is a function for calculating the northeast ray
 U64 get_ray_north_east(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 	return get_ray_diagonal(square) & (-2ULL << square);
 }
 
 // This is a function for calculating the southwest ray
 U64 get_ray_south_west(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 	return get_ray_diagonal(square) & ((1ULL << square) - 1);
 }
 
 // This is a function for calculating the anti northwest ray
 U64 get_ray_north_west(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 	return get_ray_anti_diagonal(square) & (-2ULL << square);
 }
 
 // This is a function for calculating the southeast ray
 U64 get_ray_south_east(Square square) {
-	assert(square < SQUARE_NB);
+	assert(square < SQ_NB);
 	return get_ray_anti_diagonal(square) & ((1ULL << square) - 1);
 }
