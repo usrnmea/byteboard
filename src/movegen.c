a5e89bcd4cbea48d19bdaeedfea4a77ad08d8157
Gaiercop <gaiercop@gmail.com>
2022-05-31 12:06:43 +0300

    movegen.c: add_common_moves and add_castlings realisations

--- src/movegen.c
+++ src/movegen.c
@@ -24,3 +24,57 @@ MoveList *init_move_list(void)
 
     return move_list;
 }
+
+void add_common_moves(
+	MoveList *move_list,
+	const Piece piece,
+	const Square source,
+	U64 destinations
+)
+{
+	assert(move_list != NULL);
+	assert(source < SQ_NB);
+	assert(piece != NO_PIECE);
+
+	Move move = {
+		.move_type = COMMON, .moved_piece_type = type_of_piece(piece),
+		.promotion_piece_type = NO_PIECE_TYPE, .color = color_of_piece(piece),
+		.source = source
+	};
+
+	ExtMove ext_move = {.move = move, .eval = NO_EVAL};
+
+	while (destinations) {
+		ext_move.move.destination = bit_scan_forward(destinations);
+
+		ml_add(move_list, ext_move);
+
+		remove_lsb(destinations);
+	}
+}
+
+void add_castlings(
+	MoveList *move_list,
+	const Square source,
+	U64 destinations
+)
+{
+	assert(move_list != NULL);
+	assert(source < SQ_NB);
+
+	Move move = {
+		.move_type = CASTLING, .moved_piece_type = KING,
+		.promotion_piece_type = NO_PIECE_TYPE,
+		.color = source == SQ_E8, .source = source
+	};
+
+	ExtMove ext_move = {.move = move, .eval = NO_EVAL};
+
+	while (destinations) {
+		ext_move.move.destination = bit_scan_forward(destinations);
+
+		ml_add(move_list, ext_move);
+
+		remove_lsb(destinations);
+	}
+}
