0f424b06d5d1479db4dbef64dfcd31c6ee235471
usrnmea <53175704+usrnmea@users.noreply.github.com>
2022-06-01 23:19:29 +0300

    evaluate.c: evaluate_material unit tests

---
+++ test/test_evaluate.c
@@ -0,0 +1,32 @@
+#include "unity.h"
+#include "bitboard.h"
+#include "bitboard_mapping.h"
+#include "piece.h"
+#include "rays.h"
+#include "patterns.h"
+#include "masks.h"
+#include "position.h"
+#include "evaluate.h"
+
+#include <stdlib.h>
+
+/// Because of the constant change in evaluations, tests are very difficult to do.
+/// That's why testing is done on the most basic things. For example, if there
+/// is an obvious white advantage, the function does not return DRAW or less.
+
+void test_evaluate_material(void)
+{
+	Position *pos = init_position("8/1nbk4/8/8/8/8/1QRK4/8 w - - 0 1");
+
+	TEST_ASSERT_GREATER_THAN(DRAW, evaluate_material(pos));
+
+	free(pos->state);
+	free(pos);
+
+	pos = init_position("8/1qrk4/8/8/8/8/1BNK4/8 w - - 0 1");
+
+	TEST_ASSERT_LESS_THAN(DRAW, evaluate_material(pos));
+
+	free(pos->state);
+	free(pos);
+}
