af386fde6e3dfcf8f18e8071fd2c36c4de6675ca
Герман Келлер <gaiercop@gmail.com>
2022-06-23 11:35:47 +0700

    perft.h: generate_all_moves defenition was changed

--- include/perft.h
+++ include/perft.h
@@ -5,15 +5,18 @@
 #define __PERFT_H__
 
 #include "bitboard.h"
+#include "position.h"
 
 /**
  * \brief Debugging function to walk the move generation tree of strictly legal moves
  * to count all the leaf nodes of a certain depth
  *
+ * \param pos current position
+ *
  * \param depth given depth
  *
  * \return number of generated nodes
  */
-U64 perft(int depth);
+U64 perft(Position *pos, int depth);
 
 #endif
