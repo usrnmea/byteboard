4d99b13993613eab647c9bfe2cbed5997a82bee9
Герман Келлер <gaiercop@gmail.com>
2022-06-23 14:07:07 +0700

    main.c: init position was defined

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include "bitboard.h"
 #include "bitboard_mapping.h"
 #include "piece.h"
@@ -14,7 +15,17 @@ int main(void)
 {
 	init_rays();
 
-	printf("Hello, byteboard!\n");
+	Position *pos = init_position(
+		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	);
+
+	printf(
+		"Hello, byteboard!\nNodes in the start position: %llu",
+		perft_test(pos, 2)
+	);
+
+	free(pos->state);
+	free(pos);
 
 	return 0;
 }
