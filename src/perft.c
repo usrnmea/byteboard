82f467d50d78faf9dbc0e9cfd0479c5a9f32ecc6
Герман Келлер <gaiercop@gmail.com>
2022-06-23 14:07:41 +0700

    perft.c: added function to print bitboards

--- src/perft.c
+++ src/perft.c
@@ -11,14 +11,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x01 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x80 ? '1' : '0')
+
+
+void print_bitboard(U64 *bitboard)
+{
+    for(uint8_t counter = 0; counter != 8; counter++)
+        printf(BYTE_TO_BINARY_PATTERN"\n", BYTE_TO_BINARY(*((uint8_t *) bitboard + (7 - counter))));
+} // Temporary
+
 U64 perft(Position *pos, int depth)
 {
-	if (depth == 0) {
-		return 1ULL;
-	}
 
 	U64 nodes = 0;
 	MoveList *move_list = generate_all_moves(pos);
+
+	U64 n_moves = ml_len(move_list);
+
+	if (depth == 1) {
+		return n_moves;
+	}
 
 	for (int i = 0; i < ml_len(move_list); i++) {
 		do_move(pos, move_list->move_list[i].move);
@@ -40,7 +61,7 @@ U64 perft_test(Position *pos, int depth)
 	U64 nodes = 0;
 	MoveList *move_list = generate_all_moves(pos);
 
-	for (int i = 0; i < ml_len(move_list); i++) {
+	for (int i = 7; i < ml_len(move_list); i++) {
 
 		U64 cummulative_nodes = nodes;
 
