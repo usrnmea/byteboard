4be934820c64652c176954dd3f291ef7e79a2de8
usrnmea <53175704+usrnmea@users.noreply.github.com>
2022-06-24 14:55:40 +0300

    search.c | meson.build: search.c | search.c: find_best stub + get_random_move stub

--- include/search.h
+++ include/search.h
@@ -4,4 +4,27 @@
 #ifndef __SEARCH_H__
 #define __SEARCH_H__ 
 
+#include "bitboard.h"
+#include "position.h"
+#include "evaluate.h"
+#include "movegen.h"
+
+/**
+ * \brief Returns the best move according to the chess engine
+ *
+ * \param position position
+ *
+ * \return #ExtMove with the evaluation and the move
+ */
+ExtMove find_best(Position *position);
+
+/**
+ * \brief Selects one random move from the #MoveList
+ *
+ * \param move_list move list(shouldn't be empty)
+ *
+ * \return random move
+ */
+ExtMove get_random_move(MoveList *move_list);
+
 #endif
---
+++ src/search.c
@@ -0,0 +1,42 @@
+#include "bitboard.h"
+#include "position.h"
+#include "evaluate.h"
+#include "movegen.h"
+
+#include <assert.h>
+#include <stdlib.h>
+
+ExtMove find_best(Position *position)
+{
+	assert(position != NULL);
+
+	return (ExtMove) {
+		.move = (Move) {
+			.move_type = COMMON,
+			.moved_piece_type = NO_PIECE_TYPE,
+			.promotion_piece_type = NO_PIECE_TYPE,
+			.color = COLOR_NB,
+			.destination = SQ_NONE,
+			.source = SQ_NONE,
+		},
+		.eval = NO_EVAL,
+	};
+}
+
+ExtMove get_random_move(MoveList *move_list)
+{
+	assert(move_list != NULL);
+	assert(ml_len(move_list) != 0);
+
+	return (ExtMove) {
+		.move = (Move) {
+			.move_type = COMMON,
+			.moved_piece_type = NO_PIECE_TYPE,
+			.promotion_piece_type = NO_PIECE_TYPE,
+			.color = COLOR_NB,
+			.destination = SQ_NONE,
+			.source = SQ_NONE,
+		},
+		.eval = NO_EVAL,
+	};
+}
