49416b0612678b227acf16f5b2555005192aef49
usrnmea <53175704+usrnmea@users.noreply.github.com>
2022-06-24 15:23:35 +0300

    search.c: get_random_move realisation

--- src/search.c
+++ src/search.c
@@ -5,6 +5,7 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 
 ExtMove find_best(Position *position)
 {
@@ -28,15 +29,9 @@ ExtMove get_random_move(MoveList *move_list)
 	assert(move_list != NULL);
 	assert(ml_len(move_list) != 0);
 
-	return (ExtMove) {
-		.move = (Move) {
-			.move_type = COMMON,
-			.moved_piece_type = NO_PIECE_TYPE,
-			.promotion_piece_type = NO_PIECE_TYPE,
-			.color = COLOR_NB,
-			.destination = SQ_NONE,
-			.source = SQ_NONE,
-		},
-		.eval = NO_EVAL,
-	};
+	srand(time(NULL));
+
+	uint32_t i = rand() % ml_len(move_list);
+
+	return move_list->move_list[i];
 }
