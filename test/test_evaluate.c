f38ccf2e24ccfbe84b4e2af21614791de9e2d145
usrnmea <53175704+usrnmea@users.noreply.github.com>
2022-06-27 14:42:19 +0300

    evaluate.c: evaluate_central_pawns unit tests

--- test/test_evaluate.c
+++ test/test_evaluate.c
@@ -48,3 +48,20 @@ void test_evaluate_material(void)
 	free(pos->state);
 	free(pos);
 }
+
+void test_evaluate_central_pawns(void)
+{
+	Position *pos = init_position("K7/8/8/4p3/3PP3/2P5/8/k7 w - - 0 1")
+
+	TEST_ASSERT_GREATER_THAN(DRAW, evaluate_material(pos));
+
+	free(pos->state);
+	free(pos);
+
+	pos = init_position("K7/8/2p2p2/3pp3/4P3/2P5/8/k7 w - - 0 1")
+
+	TEST_ASSERT_LESS_THAN(DRAW, evaluate_material(pos));
+
+	free(pos->state);
+	free(pos);
+}
