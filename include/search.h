0b26707c74e457ed2e4869c0ee728c32942726a4
Герман Келлер <gaiercop@gmail.com>
2022-06-29 14:51:20 +0700

    search.h: evaluation_move defenition

--- include/search.h
+++ include/search.h
@@ -12,6 +12,16 @@
 /// Counter for the number of nodes
 extern U64 nodes;
 
+/// MVV LVA table
+static const Evaluation mvv_lva[6][6] = {
+	{105, 355, 355, 525, 1005, 100005},
+	{104, 354, 354, 524, 1004, 100004},
+	{103, 353, 353, 523, 1003, 100003},
+	{102, 352, 352, 522, 1002, 100002},
+	{101, 351, 351, 521, 1001, 100001},
+	{100, 350, 350, 520, 1000, 100000}
+};
+
 /**
  * \brief Returns the best move according to the chess engine
  *
@@ -31,6 +41,15 @@ ExtMove find_best(Position *position, uint32_t depth);
  * \return random move
  */
 ExtMove get_random_move(MoveList *move_list);
+
+/**
+ * \brief Evaluates the given move.
+ *
+ * \param pos current position
+ *
+ * \param move move to be evaluated
+ */
+void evaluate_move(Position *pos, ExtMove *move);
 
 /**
  * \brief Function which realises quiescence search algorithm.
